060afca9e03ce33b95951f08c9f9081e55a487be
Dan Hoeger <dan.hoeger@outlook.com>
2018-02-27 11:38:12 -0600

    update BeyoundBehaviorSchedule

--- BeyondBehaviorSchedule/BeyondBehaviorSchedule/MainWindow.xaml.cs
+++ BeyondBehaviorSchedule/BeyondBehaviorSchedule/MainWindow.xaml.cs
@@ -1,43 +1,67 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Xml;
+using System.Xml.Linq;
 
-namespace BeyondBehaviorSchedule
+namespace LinqToXmlDataBinding
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    /// <summary>  
+    /// Interaction logic for L2XDBForm.xaml  
+    /// </summary>  
+
+    public partial class L2XDBForm : System.Windows.Window
     {
-        public MainWindow()
+        XNamespace mybooks = "http://www.mybooks.com";
+        XElement bookList;
+
+        public L2XDBForm()
         {
             InitializeComponent();
-            XmlDataProvider ClientDefault = new XmlDataProvider();
-            ClientDefault.Document.Load("ClientDefault.xml");
+            bookList = XElement.Load("books.xml").Element("books");
+            //((ObjectDataProvider)Resources["LoadedBooks"]).ObjectInstance = bookList;
+            //bookList = (XElement)((ObjectDataProvider)Resources["LoadedBooks"]).Data;
         }
 
-        private void WeekStarting_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        void OnRemoveBook(object sender, EventArgs e)
         {
-            
-            if(this.WeekStarting.SelectedDate.Value.DayOfWeek > DayOfWeek.Monday)
+            int index = lbBooks.SelectedIndex;
+            if (index < 0) return;
+
+            XElement selBook = (XElement)lbBooks.SelectedItem;
+            //Get next node before removing element.  
+            XNode nextNode = selBook.NextNode;
+            selBook.Remove();
+
+            //Remove any matching newline node.  
+            if (nextNode != null && nextNode.ToString().Trim().Equals(""))
+            { nextNode.Remove(); }
+
+            //Set selected item.   
+            if (lbBooks.Items.Count > 0)
+            { lbBooks.SelectedItem = lbBooks.Items[index > 0 ? index - 1 : 0]; }
+        }
+
+        void OnAddBook(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(tbAddID.Text) ||
+                String.IsNullOrEmpty(tbAddValue.Text))
             {
-                this.WeekStarting.SelectedDate = this.WeekStarting.SelectedDate.Value.AddDays((double)(-1 * ((int)this.WeekStarting.SelectedDate.Value.DayOfWeek - 1)));
+                MessageBox.Show("Please supply both a Book ID and a Value!", "Entry Error!");
+                return;
             }
-            else if(this.WeekStarting.SelectedDate.Value.DayOfWeek == DayOfWeek.Sunday)
-            {
-                this.WeekStarting.SelectedDate = this.WeekStarting.SelectedDate.Value.AddDays(1);
-            }
+            XElement newBook = new XElement(
+                                mybooks + "book",
+                                new XAttribute("id", tbAddID.Text),
+                                tbAddValue.Text);
+            bookList.Add("  ", newBook, "\r\n");
         }
     }
 }
---
+++ BeyondBehaviorSchedule/BeyondBehaviorScheduleApp/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Nevron.Nov.Schedule;
+using Nevron.Nov.UI;
+using Nevron.Nov.Windows;
+
+namespace BeyondBehaviorScheduleApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Set window size
+            this.Width = 800;
+            this.Height = 600;
+
+            // Create NOV content
+            NNovWidgetHost<NScheduleViewWithRibbon> novContent = new NNovWidgetHost<NScheduleViewWithRibbon>();
+            Content = novContent;
+
+            // Add appointments
+            AddAppointments(novContent.Widget.View);
+        }
+
+        private void AddAppointments(NScheduleView scheduleView)
+        {
+            // Pause history service to prevent creating undo actions for the added content
+            scheduleView.Document.PauseHistoryService();
+
+            // Get the schedule
+            NSchedule schedule = scheduleView.Content;
+
+            // TODO: add appointments
+            DateTime today = DateTime.Today;
+            schedule.Appointments.Add(new NAppointment("Meeting with John", today.AddHours(9), today.AddHours(10)));
+            schedule.Appointments.Add(new NAppointment("Lunch", today.AddHours(12), today.AddHours(13)));
+            schedule.Appointments.Add(new NAppointment("Video Presentation", today.AddHours(14), today.AddHours(16)));
+
+            // Resume history service
+            scheduleView.Document.ResumeHistoryService();
+        }
+    }
+}
